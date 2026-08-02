52f800286fd0c2ddccb23bde85107b278627f71a
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2026-08-02 10:30:32 +0300

    zylle`s plant ext

---
+++ Source/BiomesCore/BiomesCore/DefModExtensions/PlantHarvestExtension.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace BiomesCore.DefModExtensions
+{
+
+    public class PlantHarvestExtension : DefModExtension
+    {
+        public ThoughtDef memory = null;
+
+		public List<ExtraHarvest> extraHarvests = new List<ExtraHarvest>();
+
+		public class ExtraHarvest
+		{
+			public ThingDef harvestedThingDef;
+			public float harvestYield = 1f;
+			public float harvestChance = 1f;
+			public float harvestMinGrowth = 0.65f;
+			public int minSkillLevel = 0;
+		}
+	}
+
+	[Obsolete("Please use PlantHarvestExtension instead.")]
+	public class PlantHarvestMemoryExtension : PlantHarvestExtension
+	{
+
+	}
+
+}
--- Source/BiomesCore/BiomesCore/Patches/Plants/Plant_PlantCollected_Patch.cs
+++ Source/BiomesCore/BiomesCore/Patches/Plants/Plant_PlantCollected_Patch.cs
@@ -1,6 +1,7 @@
 ﻿using BiomesCore.DefModExtensions;
 using HarmonyLib;
 using RimWorld;
+using System;
 using Verse;
 
 namespace BiomesCore.Patches.Plants
@@ -10,13 +11,46 @@ namespace BiomesCore.Patches.Plants
     {
         public static void Postfix(Pawn by, Plant __instance)
         {
-            if (__instance.def.HasModExtension<PlantHarvestMemoryExtension>())
-            {
-                var plantHarvestMemoryExtension =
-                    __instance.def.GetModExtension<PlantHarvestMemoryExtension>();
-                by?.needs?.mood?.thoughts?.memories.TryGainMemory(plantHarvestMemoryExtension
-                    .memory);
-            }
-        }
+			PlantHarvestExtension plantExt = __instance.def?.GetModExtension<PlantHarvestExtension>();
+			if (plantExt != null)
+			{
+				if (plantExt.memory != null)
+				{
+					by?.needs?.mood?.thoughts?.memories.TryGainMemory(plantExt.memory);
+				}
+				if (!plantExt.extraHarvests.NullOrEmpty())
+				{
+					foreach (PlantHarvestExtension.ExtraHarvest extH in plantExt.extraHarvests)
+					{
+						if (Rand.Value > extH.harvestChance)
+						{
+							//Log.Message("no harvest - random");
+							continue;
+						}
+						if (__instance.Growth < extH.harvestMinGrowth)
+						{
+							//Log.Message("no harvest - immature");
+							continue;
+						}
+						if (extH.minSkillLevel > 0 && (by.skills == null || by.skills.GetSkill(SkillDefOf.Plants).GetLevel() < extH.minSkillLevel))
+						{
+							//Log.Message("no harvest - unskilled");
+							continue;
+						}
+
+						// if it's valid for the 2n harvest to spawn
+						Thing thing = ThingMaker.MakeThing(extH.harvestedThingDef);
+						thing.stackCount = (int)extH.harvestYield;
+						if (by.Faction != Faction.OfPlayer)
+						{
+							thing.SetForbidden(value: true);
+						}
+						GenPlace.TryPlaceThing(thing, by.Position, by.Map, ThingPlaceMode.Near);
+
+					}
+				}
+
+			}
+		}
     }
 }
